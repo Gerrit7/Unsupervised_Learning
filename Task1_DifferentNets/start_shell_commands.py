eea88776ea03c2675f02306174679a95c6ddbcff
Gerrit <gerrit.alexander@web.de>
2020-12-15 11:17:49 +0100

    Before Training every Dataset with 0.1 steps of train data

---
+++ Task1_DifferentNets/start_shell_commands.py
@@ -0,0 +1,11 @@
+import os
+
+slices = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+os.system('python train.py '+
+'--data_name retinamnist ' +
+'--input_root ../../DataSets/medmnist-20201211T084149Z-001/medmnist/ ' +
+'--output_root ../../TrainedNets/Training_Resnet18_self ' +
+'--num_epoch 100 ' +
+'--train_size 0.5 ' +
+'--download False '
+)
