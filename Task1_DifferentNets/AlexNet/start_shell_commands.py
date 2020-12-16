3fb9bffcd5662f3ff30db8b2c8f24cfc0abe671b
Gerrit <gerrit.alexander@web.de>
2020-12-16 18:39:14 +0100

    New Folder Structure for Task1

--- Task1_DifferentNets/AlexNet/start_shell_commands.py
+++ Task1_DifferentNets/AlexNet/start_shell_commands.py
@@ -9,7 +9,7 @@ dataSets3   = ["organmnist_axial", "organmnist_coronal", "organmnist_sagittal"]
 
 for dset in range(len(dataSets)):
     for slc in slices:
-        os.system('python trainResNet.py '+
+        os.system('python train.py '+
         '--data_name ' + str(dataSets[dset]) + ' ' +
         '--input_root ../../DataSets/medmnist-20201211T084149Z-001/medmnist/ ' +
         '--output_root ../../TrainedNets/Training_Resnet18_self ' +
