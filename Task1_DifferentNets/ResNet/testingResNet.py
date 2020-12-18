a0ecc0be8cba831241840231ad32c7bbaf19f41d
Gerrit <gerrit.alexander@web.de>
2020-12-18 18:10:27 +0100

    testingResNet working

--- Task1_DifferentNets/ResNet/testingResNet.py
+++ Task1_DifferentNets/ResNet/testingResNet.py
@@ -16,7 +16,7 @@ from medmnist.evaluator import getAUC, getACC, save_results
 from medmnist.info import INFO
 
 
-def main(flag, input_model):
+def main(flag, dataset_root, model_root, trainSize, download):
     ''' main function
     :param flag: name of subset
 
@@ -49,55 +49,74 @@ def main(flag, input_model):
     epoch_old = 0
     auc_old = 0
 
-    if trainSize != 1:
-        flag = flag + "_" + str(trainSize)
-
-    dir_path = os.path.join(output_root, '%s_checkpoints' % (flag))
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-
     print('==> Preparing data...')
-
-    test_transform = transforms.Compose(
-        [transforms.Resize(256),
-        transforms.CenterCrop(224),transforms.ToTensor(),
+    train_transform = transforms.Compose(
+        [transforms.ToTensor(),
          transforms.Normalize(mean=[.5], std=[.5])])
 
-    test_dataset = DataClass(root=input_root,
+    val_transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize(mean=[.5], std=[.5])])
+
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize(mean=[.5], std=[.5])])
+
+    train_dataset = DataClass(root=dataset_root,
+                                    split='train',
+                                    transform=train_transform,
+                                    download=download)
+
+    indices = torch.randperm(len(train_dataset))[:round(len(train_dataset)*trainSize)]
+    for idx in indices:
+        train_dataset_scaled.append(train_dataset[idx])
+
+    train_loader = data.DataLoader(dataset=train_dataset_scaled,
+                                   batch_size=batch_size,
+                                   shuffle=True)
+    val_dataset = DataClass(root=dataset_root,
+                                  split='val',
+                                  transform=val_transform,
+                                  download=download)
+    val_loader = data.DataLoader(dataset=val_dataset,
+                                 batch_size=batch_size,
+                                 shuffle=True)
+    test_dataset = DataClass(root=dataset_root,
                                    split='test',
                                    transform=test_transform,
                                    download=download)
     test_loader = data.DataLoader(dataset=test_dataset,
                                   batch_size=batch_size,
                                   shuffle=True)
-    print('Test: ', len(test_dataset))
-    
-    
-    print('==> Testing model...')
-    restore_model_path = os.path.join(
-        dir_path, 'ckpt_%d_auc_%.5f.pth' % (index, auc_list[index]))
 
-    model.load_state_dict(torch.load(restore_model_path)['net'])
+    print('Train: ', len(train_dataset_scaled), ', Valid: ', len(val_dataset), ', Test: ', len(test_dataset))
+    
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = ResNet18(in_channels=n_channels, num_classes=n_classes)
+
+    print('==> Testing model...')
+
+    for file in os.listdir(os.path.join(model_root,'%s_%.2f' % (flag, trainSize))):
+        if file.endswith(".pth"):
+            restore_model_path = os.path.join(os.path.join(model_root,'%s_%.2f' % (flag, trainSize)),file)
+    model.to(device)
+    model.load_state_dict(torch.load(restore_model_path, map_location=device)['net'])
+
     test(model,
             'train',
             train_loader,
             device,
             flag,
             task,
-            output_root=output_root)
-    test(model, 'val', val_loader, device, flag, task, output_root=output_root)
+            output_root=model_root)
+    test(model, 'val', val_loader, device, flag, task, output_root=model_root)
     test(model,
             'test',
             test_loader,
             device,
             flag,
             task,
-            output_root=output_root)
-
-    save_best_model_path = os.path.join( 
-        os.path.join(output_root, flag), 'ckpt_%d_auc_%.5f.pth' % (index, auc_list[index]))
-    copyfile(restore_model_path, save_best_model_path)
-    shutil.rmtree(dir_path)
+            output_root=model_root)
 
 def test(model, split, data_loader, device, flag, task, output_root=None):
     ''' testing function
@@ -137,12 +156,12 @@ def test(model, split, data_loader, device, flag, task, output_root=None):
         acc = getACC(y_true, y_score, task)
         print('%s AUC: %.5f ACC: %.5f' % (split, auc, acc))
 
-        if output_root is not None:
+        """ if output_root is not None:
             output_dir = os.path.join(output_root, flag)
             if not os.path.exists(output_dir):
                 os.mkdir(output_dir)
             output_path = os.path.join(output_dir, '%s.csv' % (split))
-            save_results(y_true, y_score, output_path)
+            save_results(y_true, y_score, output_path) """
 
 
 if __name__ == '__main__':
@@ -152,18 +171,14 @@ if __name__ == '__main__':
                         default='pathmnist',
                         help='subset of MedMNIST',
                         type=str)
-    parser.add_argument('--input_root',
-                        default='./input',
+    parser.add_argument('--dataset_root',
+                        default='./dataset',
                         help='input root, the source of dataset files',
                         type=str)
-    parser.add_argument('--output_root',
-                        default='./output',
-                        help='output root, where to save models and results',
+    parser.add_argument('--model_root',
+                        default='./model_root',
+                        help='model root, where the models and results are saved',
                         type=str)
-    parser.add_argument('--num_epoch',
-                        default=100,
-                        help='num of epochs of training',
-                        type=int)
     parser.add_argument('--train_size',
                         default=1.0,
                         help='size of trainingdata',
@@ -175,14 +190,13 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     data_name = args.data_name.lower()
-    input_root = args.input_root
-    output_root = args.output_root
-    end_epoch = args.num_epoch
+    dataset_root = args.dataset_root
+    model_root = args.model_root
     train_size = args.train_size
     download = args.download
-    main(data_name,
-         input_root,
-         output_root,
-         end_epoch=end_epoch,
-         trainSize = train_size,
-         download=download)
+    
+    main(data_name, 
+        dataset_root, 
+        model_root, 
+        trainSize = train_size,
+        download=download)
